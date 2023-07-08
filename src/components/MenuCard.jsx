092e002f049f6f98e0cafaf2f84e02f285e6034c
swastikpatro <patroswastik.jsdev@gmail.com>
2023-07-08 23:02:20 +0530

    fix: navigate on click on cards

--- src/components/MenuCard.jsx
+++ src/components/MenuCard.jsx
@@ -14,33 +14,42 @@ const MenuCard = ({ menuData, restaurantInfo }) => {
   const { name, imgSrc, price, qty } = menuData;
   return (
     <Card as="article" borderRadius={'md'} p="0" boxShadow={'md'}>
-      <Box
-        as="div"
-        h="15rem"
-        overflow={'hidden'}
-        borderRadius={'.5rem .5rem 0 0'}
+      <ChakraLink
+        as={Link}
+        to={`/restaurant/${restaurantInfo.id}`}
+        _hover={{ textDecoration: 'none' }}
+        color="gray.500"
+        letterSpacing={'wider'}
       >
-        <Image w="full" h="full" objectFit={'cover'} src={imgSrc} alt={name} />
-      </Box>
-
-      <CardBody>
-        <Heading as="h3" fontSize="1.2rem" mb=".5rem" letterSpacing={'wider'}>
-          {name}
-        </Heading>
-
-        <Text color="gray.500" letterSpacing={'wider'} mb=".5rem">
-          Rs. {price} for {qty}
-        </Text>
-
-        <ChakraLink
-          as={Link}
-          to={`/restaurant/${restaurantInfo.id}`}
-          color="gray.500"
-          letterSpacing={'wider'}
+        <Box
+          as="div"
+          h="15rem"
+          overflow={'hidden'}
+          borderRadius={'.5rem .5rem 0 0'}
         >
-          {restaurantInfo.name}
-        </ChakraLink>
-      </CardBody>
+          <Image
+            w="full"
+            h="full"
+            objectFit={'cover'}
+            src={imgSrc}
+            alt={name}
+          />
+        </Box>
+
+        <CardBody>
+          <Heading as="h3" fontSize="1.2rem" mb=".5rem" letterSpacing={'wider'}>
+            {name}
+          </Heading>
+
+          <Text color="gray.500" letterSpacing={'wider'} mb=".5rem">
+            Rs. {price} for {qty}
+          </Text>
+
+          <Text color="gray.500" letterSpacing={'wider'} mb=".5rem">
+            {restaurantInfo.name}
+          </Text>
+        </CardBody>
+      </ChakraLink>
     </Card>
   );
 };
